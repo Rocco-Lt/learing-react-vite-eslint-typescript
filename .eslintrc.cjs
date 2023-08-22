68b6ad6aeeb6a7dd048c8544f43d5aacf981363a
rocco.liu <2863433495@qq.com>
2023-08-22 23:00:07 +0800

    ci: 集成: 🎡 增加styleLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,21 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "plugin:prettier/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier', 'react-hooks', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     'no-undef': 'off',
+    "react-hooks/rules-of-hooks": "off",
+    "react-hooks/exhaustive-deps": "off"
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import viteEslint from 'vite-plugin-eslint';
+import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,5 +12,17 @@ export default defineConfig({
       //  exclue: ['./node_modules/**'],
       cache: false
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        math: 'parens-division'
+      }
+    }
+  }
 });
